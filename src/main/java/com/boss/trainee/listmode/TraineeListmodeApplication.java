d956e3513a36021725676adf4ed1e66b5406af3f
syf <2282763709@qq.com>
2020-07-11 18:55:21 +0800

    第二次提交

---
+++ src/main/java/com/boss/trainee/listmode/TraineeListmodeApplication.java
@@ -0,0 +1,13 @@
+package com.boss.trainee.listmode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraineeListmodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraineeListmodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boss/trainee/listmode/TraineeListmodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boss.trainee.listmode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraineeListmodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
